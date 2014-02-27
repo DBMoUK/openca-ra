70324ff2e2276bf3fc05081a8c995a71ab120064
David Bryant-Moore <davidbryant-moore@Davids-MacBook-Pro-2.local>
2014-02-27 15:45:49 +0000

    Fixed openca-ra/manifests/vhosts.pp - /var/www/html now ensured as directory.

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -2,7 +2,7 @@ class openca-ra::vhost {
   include apache
 
   file { '/var/www/html':
-    ensure  => present,
+    ensure  => directory,
     owner   => 'openca',
     group   => 'openca',
     require => User['openca'],
